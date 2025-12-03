b28147be17450e2c99babd55a9e39577735faff0
FSD9-Wave <loomaflex44@gmail.com>
2025-12-03 14:56:21 +0700

    feat: update CORS configuration in SecurityConfig to allow access from production frontend
    - Added production frontend URL to allowed origins in CORS configuration, enabling cross-origin requests from the deployed application.

--- src/main/java/com/techup/travel_app/config/SecurityConfig.java
+++ src/main/java/com/techup/travel_app/config/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(java.util.List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(java.util.List.of("http://localhost:5173","https://travel-app-frontend-jade.vercel.app"));
         configuration.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(java.util.List.of("*"));
         configuration.setExposedHeaders(java.util.List.of("Authorization", "Content-Type"));
